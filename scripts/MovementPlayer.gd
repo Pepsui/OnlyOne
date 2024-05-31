023c042c7ad9b6c0d155dbef212282ad034c80fe
Tyus Dedner <tyus.dedner12@gmail.com>
2024-05-31 01:31:39 -0500

    Initial attempts of trying to think everything out right

---
+++ scripts/MovementPlayer.gd
@@ -0,0 +1,20 @@
+class_name MovementPlayer extends Node
+
+var input_time: Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func move_right(velocity):
+	pass
+
+
+func move_left(velocity):
+	pass
